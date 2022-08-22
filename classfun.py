2b213cbdee80d831648792781f09d897ff0fe02e
RASHADK <110896723+RASHADK@users.noreply.github.com>
2022-08-22 14:18:37 +0530

    first

---
+++ classfun.py
@@ -0,0 +1,9 @@
+class rashad:
+    attr1="Rashad"
+    attr2="22"
+    def qualification(self):
+        print("My name is ",self.attr1)
+        print("My age is ",self.attr2)
+Student=rashad()
+print(Student.attr1)
+Student.qualification()
